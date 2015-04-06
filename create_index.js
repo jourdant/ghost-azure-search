ca2c48a4fa7d7fa2721b9be15cf8d6b23dcf9d58
Jourdan Templeton <j@en.vc>
2015-04-06 21:06:39 +1200

    Updated scripts to use latest Azure Search version. Generalised to work with any Azure Search index and Ghost Blog.

--- create_index.js
+++ create_index.js
@@ -1,32 +1,47 @@
-//create request, allow cookies
+//import request library, add settings
 var request = require('request');
-var index_url = "https://<domain>.search.windows.net/indexes/?api-version=2015-02-28";
-var key = "<apikey>";
-
-var index = {name: "blog",
-			  fields: [
-			  	//generic fields
-			  	{ name: "id", type: "Edm.String", "key": true },
-				{ name: "image", type: "Edm.String", "key": false, retrievable: true },
-			  	{ name: "slug", type: "Edm.String", "key": false },
-			  	{ name: "title", type: "Edm.String", "key": false, searchable: true, suggestions: true, retrievable: true },
-			  	{ name: "type", type: "Edm.String", "key": false, searchable: true, suggestions: true, retrievable: true },
-
-			  	//article specific
-			  	{ name: "text", type: "Edm.String", "key": false, searchable: true, retrievable: true },
-			  	{ name: "published_at", type: "Edm.DateTimeOffset", "key": false, retrievable: true, filterable: true, sortable: true }
-			  ],
-
-			  suggesters: [
-				{ name: "sg", searchMode: "analyzingInfixMatching", sourceFields: ["title", "text"] }
-			  ],
-
-			  corsOptions: {
-			  	allowedOrigins: ["*"]
-			  }
-			};
+var azure_index = "<indexname>";
+var azure_domain = "<searchservicename>";
+var azure_api_key = "<apikey>";
+var azure_url = "https://" + azure_domain + ".search.windows.net/indexes/" + azure_index + "?api-version=2015-02-28";
 
 
-request.post(index_url, {json: index, headers: {"api-key": key}}).on("response", function (resp) { 
-	console.log(resp.statusCode);
-});
+
+//build index payload
+var index = {
+	name: azure_index,
+	fields: [
+		//generic fields
+		{ name: "id", 			type: "Edm.String", "key": true },
+		{ name: "image", 		type: "Edm.String" },
+		{ name: "slug", 		type: "Edm.String" },
+		{ name: "title", 		type: "Edm.String", searchable: true},
+		{ name: "type", 		type: "Edm.String", searchable: true, filterable: true},
+		{ name: "tags", 		type: "Collection(Edm.String)"},
+
+		//article specific
+		{ name: "text", 		type: "Edm.String", searchable: true, analyzer: "en.lucene"},
+		{ name: "published_at", type: "Edm.DateTimeOffset", filterable: true, sortable: true }
+	],
+
+	suggesters: [
+		{ name: "sg", searchMode: "analyzingInfixMatching", sourceFields: ["title"] }
+	],
+
+	corsOptions: {
+		allowedOrigins: ["*"]
+	}
+};
+
+
+//create index in azure
+request.put(azure_url, {json: index, headers: {"api-key": azure_api_key}})
+	.on("response", function (resp) {
+		if (resp.statusCode == 201) { console.log("Index created."); }
+		else if (resp.statusCode >= 200 && resp.statusCode < 210) { console.log("Index updated."); }
+		else if (resp.statusCode >= 400) { console.log(resp); }
+	})
+	.on("error", function (err)
+	{
+		console.log(err);
+	});
--- update_index.js
+++ update_index.js
@@ -1,20 +1,23 @@
-//settings
-var ghost_base_url = "http://yourghostblog.com";
-var ghost_username = "yourghostlogin";
-var ghost_password = "yourghostghost_password";
-
-var azure_index = "blog";
-var azure_domain = "yourazuresearchdomain";
-var azure_api_key = "apikey";
-var azure_url = "https://" + azure_domain + ".search.windows.net/indexes/" + azure_index + "/docs/index?api-version=2015-02-28";
-
 //import modules
 var request = require('request').defaults({jar: true});
 var htmlToText = require('html-to-text');
 
-//log into page
+//azure settings
+var request = require('request');
+var azure_index = "<indexname>";
+var azure_domain = "<searchservicename>";
+var azure_api_key = "<apikey>";
+var azure_url = "https://" + azure_domain + ".search.windows.net/indexes/" + azure_index + "/docs/index?api-version=2015-02-28";
+
+//ghost settings
+var ghost_base_url = "http://yourghostblog.com";
+var ghost_username = "<username>";
+var ghost_password = "<password>";
+
+
+//log into ghost
 var login_url = ghost_base_url + "/ghost/api/v0.1/authentication/token";
-var login_data = { grant_type: "ghost_password", ghost_username: ghost_username, ghost_password: ghost_password, client_id: "ghost-admin" }
+var login_data = { grant_type: "password", username: ghost_username, password: ghost_password, client_id: "ghost-admin" }
 console.log("Logging in");
 request.post(login_url, {form: login_data}, function(err, resp, body) {
 	if (!err) {
@@ -47,26 +50,6 @@ request.post(login_url, {form: login_data}, function(err, resp, body) {
 				}
 
 
-				//posts
-				for (post in data.db[0].data.posts) {
-					var post = data.db[0].data.posts[post];
-					if (post.page == 0 && post.status == "published") {
-						console.log("    Adding [POST] '" + post.title + "'");
-						index.value[index.value.length] = {
-							"@search.action": "upload",
-							id: 	post.uuid,
-							image: 	post.image,
-							slug: 	post.slug,
-							title: 	post.title,
-							type: 	"POST",
-
-							text: htmlToText.fromString(post.html, {wordwrap: null}),
-							published_at: new Date(post.published_at)
-						}
-					}
-				}
-
-
 				//tags
 				for (tag in data.db[0].data.tags) {
 					var tag = data.db[0].data.tags[tag];
@@ -86,8 +69,43 @@ request.post(login_url, {form: login_data}, function(err, resp, body) {
 				}
 
 
+				//posts
+				for (post in data.db[0].data.posts) {
+					var post = data.db[0].data.posts[post];
+					if (post.page == 0 && post.status == "published") {
+						//get tags for post
+						var tags = [];
+						for (posttag in data.db[0].data.posts_tags) {
+							var posttag = data.db[0].data.posts_tags[posttag];
+							if (posttag.post_id == post.id) {
+								for (tag in data.db[0].data.tags) {
+									var tag = data.db[0].data.tags[tag];
+									if (tag.id == posttag.tag_id) {
+										tags.push(tag.name);
+										break;
+									}
+								}
+							}
+						}
+
+						console.log("    Adding [POST] '" + post.title + "'");
+						index.value[index.value.length] = {
+							"@search.action": "upload",
+							id: 	post.uuid,
+							image: 	post.image,
+							slug: 	post.slug,
+							title: 	post.title,
+							type: 	"POST",
+							tags:	tags, 
+
+							text: htmlToText.fromString(post.html, {wordwrap: null}),
+							published_at: new Date(post.published_at)
+						}
+					}
+				}
+
 				console.log("Index compiled. Uploading.");
-				request.post(azure_url, {json: index, headers: {"api-azure_api_key": azure_api_key}}, function(err, resp, body) {
+				request.post(azure_url, {json: index, headers: {"api-key": azure_api_key}}, function(err, resp, body) {
 					if (resp.statusCode == 200) {console.log("Upload complete.");}
 					else {console.log(resp.statusCode);}
 				});
